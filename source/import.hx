66a6253e35df1e119f48973b893b2b2f7a40bd64
affsvoyo <githubunoficial@gmail.com>
2026-03-14 20:56:04 -0300

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,1 +1,73 @@
+#if !macro
 import Paths;
+
+#if sys
+import sys.*;
+import sys.io.*;
+#elseif js
+import js.html.*;
+#end
+
+// Mobile Things
+#if TOUCH_CONTROLS
+import mobile.flixel.*;
+import mobile.states.*;
+import mobile.objects.*;
+import mobile.options.*;
+import mobile.backend.*;
+import mobile.psychlua.*;
+import mobile.substates.*;
+import mobile.objects.Hitbox;
+import mobile.objects.MobilePad;
+import mobile.backend.MobileData;
+#else
+import mobile.backend.StorageUtil;
+import mobile.backend.PsychJNI;
+import mobile.options.*;
+import mobile.backend.MobileScaleMode;
+#end
+
+// Android
+#if android
+import android.Tools as AndroidTools;
+import android.Settings as AndroidSettings;
+import android.widget.Toast as AndroidToast;
+import android.content.Context as AndroidContext;
+import android.Permissions as AndroidPermissions;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Environment as AndroidEnvironment;
+import android.os.BatteryManager as AndroidBatteryManager;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
+
+// Lua
+#if LUA_ALLOWED
+import llua.Lua;
+import llua.LuaL;
+import llua.State;
+import llua.Convert;
+#end
+
+//Flixel
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.util.FlxTimer;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.tweens.FlxEase;
+#if !NEW_PSYCH063
+import flixel.system.FlxSound;
+#else
+import flixel.sound.FlxSound;
+#end
+import flixel.util.FlxDestroyUtil;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.graphics.frames.FlxAtlasFrames;
+#end
+
+using StringTools;
